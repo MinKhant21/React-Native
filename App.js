ffbdb1118f20af05bb17fe3c9f1817fef8c08a51
minkhant21 <kaungminkhant6889@gmail.com>
2023-04-24 22:38:21 +0630

    card

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { View ,Text } from "react-native"
+import { View ,Text,Image } from "react-native"
 import React from "react"
 const App = () => {
   return (
-    <View style={{ padding:10,height:100,width:100,margin:10,border:"1px solid green",borderRadius:10,backgroundColor:"blue" }}>
+    <View style={{ padding:"auto",height:"auto",width:200,margin:100,border:"1px solid green",borderRadius:10,backgroundColor:"blue" }}>
+      <Image source={require("./assets/favicon.png")} style={{ width:100 ,height:100}}>
+    
+      </Image>
       <Text style={{ 
         color:"white",
+        textAlign:'center'
        }}>
         kaung min kahnt
       </Text>
